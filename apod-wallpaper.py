895edfe927c3e558bfcfa8e1a36defd5e815f00e
Łukasz Kurowski <crackcomm@gmail.com>
2022-05-02 08:10:13 +0200

    fix(bg): download only non-empty links

--- apod-wallpaper.py
+++ apod-wallpaper.py
@@ -7,8 +7,9 @@ page = http.request("GET", "https://apod.nasa.gov/apod/astropix.html")
 soup = BeautifulSoup(page.data, features="lxml")
 
 x = soup.body.find_all("a")[1].get("href")
-url = "https://apod.nasa.gov/apod/%s" % x
-image = http.request("GET", url)
-f = open("bg.jpg", "wb")
-f.write(image.data)
-f.close()
+if x and x != "":
+    url = "https://apod.nasa.gov/apod/%s" % x
+    image = http.request("GET", url)
+    f = open("bg.jpg", "wb")
+    f.write(image.data)
+    f.close()
