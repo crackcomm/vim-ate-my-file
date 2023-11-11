a90db5b9a4fd4fb3e5d12b41a25903d4025bf704
Łukasz Kurowski <crackcomm@gmail.com>
2023-11-11 15:29:17 +0100

    feat(containers): run as user

--- containers/build.sh
+++ containers/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-podman build -f containers/Dockerfile -t crackcomm/dev:latest .
+podman build \
+  --build-arg UNAME=$(whoami) \
+  --build-arg UID=$(id -u) \
+  --build-arg GID=$(id -g) \
+  -f containers/Dockerfile -t crackcomm/dev:latest .
