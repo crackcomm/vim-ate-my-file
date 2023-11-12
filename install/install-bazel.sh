f8764d9df847d2091e2734a1f7fdc75602df8584
Łukasz Kurowski <crackcomm@gmail.com>
2023-11-12 18:32:06 +0100

    fix(bazel): use bazelisk instead

--- install/install-bazel.sh
+++ install/install-bazel.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-wget -O/usr/local/bin/bazel https://releases.bazel.build/6.1.0/release/bazel-6.1.0-linux-x86_64
+wget -O/usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.18.0/bazelisk-linux-amd64
 chmod +x /usr/local/bin/bazel
 
 wget -O/usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/v6.3.3/buildifier-linux-amd64
