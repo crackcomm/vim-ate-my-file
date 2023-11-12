488a014d8d82cd6b233befa645bdc740ba125f0a
Łukasz Kurowski <crackcomm@gmail.com>
2023-11-13 00:44:00 +0100

    feat(nvim/ufo): keymap descriptions

--- nvim/after/plugin/ufo.lua
+++ nvim/after/plugin/ufo.lua
@@ -1,7 +1,9 @@
+local ufo = require("ufo")
+
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-vim.keymap.set("n", "zZ", require("ufo").peekFoldedLinesUnderCursor)
+vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+vim.keymap.set("n", "zZ", ufo.peekFoldedLinesUnderCursor, { desc = "Peek folded lines under cursor" })
