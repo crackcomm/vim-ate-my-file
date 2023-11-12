698b1ff1a6629d6ad56cfffabeb90c77c73f6727
Łukasz Kurowski <crackcomm@gmail.com>
2023-11-12 23:19:30 +0100

    feat(fns.sh): `gpw` -> random pwd to clipboard

--- fns.sh
+++ fns.sh
@@ -34,3 +34,6 @@ t1() { tree -L 1 . }
 t2() { tree -L 2 . }
 t3() { tree -L 3 . }
 t4() { tree -L 4 . }
+
+# generate random password to clipboard
+gpw() { < /dev/urandom tr -dc '[:graph:]' | head -c 16 | clip }
