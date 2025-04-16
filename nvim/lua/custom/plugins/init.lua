ea84a7827be01adbfd5a526c193652d8f59f81c8
Łukasz Kurowski <crackcomm@gmail.com>
2025-04-16 08:46:24 +0200

    feat(nvim): add basic origami config

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -62,6 +62,39 @@ return {
   },
 
   {
+    "chrisgrieser/nvim-origami",
+    dependencies = { "kevinhwang91/nvim-ufo" },
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("origami").setup({
+        -- requires with `nvim-ufo`
+        keepFoldsAcrossSessions = package.loaded["ufo"] ~= nil,
+
+        pauseFoldsOnSearch = true,
+
+        -- incompatible with `nvim-ufo`
+        foldtextWithLineCount = {
+          enabled = package.loaded["ufo"] == nil,
+          template = "   %s lines", -- `%s` gets the number of folded lines
+          hlgroupForCount = "Comment",
+        },
+
+        foldKeymaps = {
+          setup = true, -- modifies `h` and `l`
+          hOnlyOpensOnFirstColumn = false,
+        },
+
+        -- redundant with `nvim-ufo`
+        autoFold = {
+          enabled = false,
+          kinds = { "comment", "imports" }, ---@type lsp.FoldingRangeKind[]
+        },
+      })
+    end,
+  },
+
+  {
     dir = "~/x/llmlsp",
     config = function()
       require("colab").setup()
