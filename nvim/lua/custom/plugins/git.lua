a160d56630397a7a24c94ca7391c87249d134c1b
Łukasz Kurowski <crackcomm@gmail.com>
2025-05-02 15:43:14 +0200

    chore(gitsigns): move to plugin file

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -12,6 +12,17 @@ return {
           untracked = { text = "┆" },
         },
       })
+
+      local overrides = require("crackcomm.themes").overrides
+
+      overrides()
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          overrides()
+        end,
+      })
     end,
   },
   -- "rhysd/git-messenger.vim",
