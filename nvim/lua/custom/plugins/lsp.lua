9ba08071400ae4b48514581e5cf51514fd802e13
Łukasz Kurowski <crackcomm@gmail.com>
2025-05-02 15:39:25 +0200

    feat(lsp): add lazydev.nvim plugin with Lua filetype and library configuration

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -56,4 +56,14 @@ return {
       require("lsp_signature").setup()
     end,
   },
+
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 }
