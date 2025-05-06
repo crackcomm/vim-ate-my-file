34ab20c4f07f10fe92f7ec151e4f3a52338b1b6b
Łukasz Kurowski <crackcomm@gmail.com>
2025-05-06 09:01:43 +0200

    feat(git): add key mappings for gitsigns functionality
    - Added on_attach function to set a key mapping for closing the diff window with `<leader>gD`.
    - Introduced navigation key mappings `[c` and `]c` for jumping to previous and next hunks, respectively, with diff-aware behavior.
    - Added `<leader>gd` mapping to invoke `diffthis()` for the current buffer.

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -2,7 +2,8 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({
+      local gs = require("gitsigns")
+      gs.setup({
         signs = {
           add = { text = "│" },
           change = { text = "│" },
@@ -11,9 +12,34 @@ return {
           changedelete = { text = "~" },
           untracked = { text = "┆" },
         },
+        on_attach = function(bufnr)
+          vim.keymap.set("n", "<leader>gD", function()
+            vim.cmd("wincmd w") -- switch to the diff window
+            vim.cmd("q") -- close it
+          end, { buffer = bufnr, desc = "[G]it [D]iff close" })
+        end,
       })
 
       vim.keymap.set("n", "<leader>gs", ":Gitsigns preview_hunk_inline<CR>", { desc = "[G]it preview hunk inline" })
+      vim.keymap.set("n", "]c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gs.nav_hunk("next")
+        end
+      end)
+
+      vim.keymap.set("n", "[c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gs.nav_hunk("prev")
+        end
+      end)
+
+      vim.keymap.set("n", "<leader>gd", function()
+        gs.diffthis()
+      end, { desc = "[G]it [D]iff" })
 
       local overrides = require("crackcomm.themes").overrides
 
