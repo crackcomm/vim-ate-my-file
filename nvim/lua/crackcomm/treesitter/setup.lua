e5a5183eaf4ee1e1c0403b88dfe9593141bea551
Łukasz Kurowski <crackcomm@gmail.com>
2025-06-17 21:12:48 +0200

    feat(treesitter): update parser list and enable indentation
    - Added "javascript", "typescript", and "rust" to `ensure_installed`.
    - Removed "javascript" from `ignore_install`.
    - Enabled `indent` and disabled it for "rust" and "ocaml".
    - Added empty `modules` table.

--- nvim/lua/crackcomm/treesitter/setup.lua
+++ nvim/lua/crackcomm/treesitter/setup.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "python" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "python", "javascript", "typescript", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -10,14 +10,14 @@ require("nvim-treesitter.configs").setup({
   auto_install = true,
 
   -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
+  ignore_install = {},
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
   indent = {
-    enable = false,
-    disable = { "python", "rust", "ocaml" },
+    enable = true,
+    disable = { "rust", "ocaml" },
   },
 
   highlight = {
@@ -67,4 +67,6 @@ require("nvim-treesitter.configs").setup({
   --     ["[]"] = "@class.outer",
   --   },
   -- },
+
+  modules = {},
 })
