e35e54ded71c8f3807ecc6ddea26584d70e81ba7
Łukasz Kurowski <crackcomm@gmail.com>
2025-11-05 22:07:17 +0100

    fix(nix-fns): add removal and copying of `/etc/nixos/apps/` in `rebuild-nixos` function
    - Deletes `/etc/nixos/apps/` directory before copying new `apps` directory
    - Copies `~/x/dot-repo/apps` to `/etc/nixos/`
    - Copies `flake.nix` and `flake.lock` to `/etc/nixos/`
    - Synchronizes `~/x/dot-repo/nix/` to `/etc/nixos/nix/`

--- scripts/nix-fns.sh
+++ scripts/nix-fns.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 rebuild-nixos() {
+  sudo rm -rf /etc/nixos/apps/
+  sudo cp -r ~/x/dot-repo/apps /etc/nixos/
   sudo cp ~/x/dot-repo/flake.{nix,lock} /etc/nixos/
   sudo rsync -avh --delete ~/x/dot-repo/nix/ /etc/nixos/nix/
   sudo nixos-rebuild switch "$@" --flake /etc/nixos#nixos-vm
