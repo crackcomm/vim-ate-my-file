b83d3b562b7800d55fd0533d8ea25fa693885127
Łukasz Kurowski <crackcomm@gmail.com>
2025-11-11 00:09:25 +0100

    style(scripts/fns.sh): fix `shfmt` formatting issues

--- scripts/fns.sh
+++ scripts/fns.sh
@@ -1,37 +1,42 @@
 #!/usr/bin/env bash
 
-cap() { read foo; printf -v tmp "$foo" }
-ret() { echo $tmp }
-rgh() { rg -e "^" -e $1 }
+cap() {
+  read foo
+  printf -v tmp "$foo"
+}
+ret() { echo $tmp; }
+rgh() { rg -e "^" -e "$1"; }
 
 # Copy to clipboard
-clip() { xclip -sel c }
+clip() { xclip -sel c; }
 
 # Copy to clipboard for a language model
 cliplm() {
   for file in "$@"; do
-    echo "File: $file\n\`\`\`\n$(cat "$file")\n\`\`\`\n" 
+    echo "File: $file"
+    echo '```'
+    cat "$file"
+    echo '```'
+    echo
   done | xclip -sel c
 }
 
 # ps -ef with interactive search
 psef() {
-  FZF_DEFAULT_COMMAND='ps -ef' \
-    fzf --bind "ctrl-r:reload(ps -ef)" \
-      --header 'Press CTRL-R to reload' --header-lines=1 \
-      --height=50% --layout=reverse
+  export FZF_DEFAULT_COMMAND='ps -ef'
+  fzf --bind "ctrl-r:reload(ps -ef)" \
+    --header 'Press CTRL-R to reload' --header-lines=1 \
+    --height=50% --layout=reverse
 }
 
 # Aliases
 scripts=$(realpath $(dirname $0))
 
-# Watch and exec respecting .gitignore.
-wx()  { $scripts/wx.sh $@ }
+wx() { $scripts/wx.sh "$@"; }
 
-t1() { tree -L 1 . }
-t2() { tree -L 2 . }
-t3() { tree -L 3 . }
-t4() { tree -L 4 . }
+t1() { tree -L 1 .; }
+t2() { tree -L 2 .; }
+t3() { tree -L 3 .; }
+t4() { tree -L 4 .; }
 
-# generate random password to clipboard
-gpw() { < /dev/urandom tr -dc '[:graph:]' | head -c 16 | clip }
+gpw() { </dev/urandom tr -dc '[:graph:]' | head -c 16 | clip; }
