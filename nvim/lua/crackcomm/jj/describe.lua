5881dc8add93cb19e572afd67faed67364261793
Łukasz Kurowski <crackcomm@gmail.com>
2025-11-22 04:41:02 +0100

    feat(jj): add progress reporter to describe command
    *   A Fidget progress reporter is initialized in `M.generate_commit_message`.
    *   Progress steps are reported during commit message generation and summary retrieval.
    *   The reporter is finalized upon completion.

--- nvim/lua/crackcomm/jj/describe.lua
+++ nvim/lua/crackcomm/jj/describe.lua
@@ -2,14 +2,22 @@ local a = require("plenary.async")
 local common = require("crackcomm.common.init")
 local jj_log = require("crackcomm.jj.log")
 
+local create_progress_reporter = require("crackcomm.progress").create_reporter
+
 local M = {}
 
 M.generate_commit_message = function(opts)
   opts = opts or {}
+
+  -- Create a Fidget progress reporter. A mock client is used for non-LSP tasks.
+  local progress = create_progress_reporter({ name = "jj" }, 2, "JJ Describe")
+
   a.run(function()
     if not opts.rev then
       opts.rev = jj_log({ rev = "::@" })
     end
+
+    progress.step("Generating commit message")
 
     local job = common.start_job({
       command = common.scripts .. "/jjcg.sh",
@@ -17,6 +25,8 @@ M.generate_commit_message = function(opts)
       enable_recording = true,
     })
     common.check_job_error(job, "generating commit message")
+
+    progress.step("Getting commit summary")
 
     local _, stdout = common.get_os_command_output({
       "jj",
@@ -30,6 +40,7 @@ M.generate_commit_message = function(opts)
 
     return stdout
   end, function(stdout)
+    progress.finish()
     if stdout then
       vim.schedule(function()
         vim.notify("jj: " .. stdout, vim.log.levels.INFO)
