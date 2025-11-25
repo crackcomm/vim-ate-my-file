cad7b64e9de37eaa54e7730ad0eb85ee26d5352a
Łukasz Kurowski <crackcomm@gmail.com>
2025-11-25 13:39:16 +0100

    feat(gopackagesdriver): export `BAZEL_WRAPPER` in `gopackagesdriver.sh`

--- nvim/scripts/gopackagesdriver.sh
+++ nvim/scripts/gopackagesdriver.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+this_script_dir="$(dirname $(realpath "$0"))"
+export BAZEL_WRAPPER="$this_script_dir/bazel_wrapper.sh"
+
 exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}" || true
