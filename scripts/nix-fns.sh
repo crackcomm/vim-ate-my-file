eedc43a9d5e914d00d2a0ec1411dac9e95f3c981
Łukasz Kurowski <crackcomm@gmail.com>
2026-04-25 07:40:24 +0200

    refactor(scripts/nix-fns): split `rebuild-nixos` into separate functions

--- scripts/nix-fns.sh
+++ scripts/nix-fns.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-rebuild-nixos() {
+copy-nixos-config() {
   sudo rm -rf /etc/nixos/apps/
   sudo cp -r ~/x/dot-repo/apps /etc/nixos/
   sudo cp ~/x/dot-repo/flake.{nix,lock} /etc/nixos/
   sudo rsync -avh --delete ~/x/dot-repo/nix/ /etc/nixos/nix/
+}
+
+rebuild-nixos() {
+  copy-nixos-config
+  sudo nixos-rebuild build "$@" --flake /etc/nixos#nixos-vm
+}
+
+rebuild-nixos-test() {
+  copy-nixos-config
+  sudo nixos-rebuild test "$@" --flake /etc/nixos#nixos-vm
+}
+
+rebuild-nixos-switch() {
+  copy-nixos-config
   sudo nixos-rebuild switch "$@" --flake /etc/nixos#nixos-vm
 }
