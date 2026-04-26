3298acfa07ae9e41301307a12f06bcc1dcf0818c
Łukasz Kurowski <crackcomm@gmail.com>
2026-04-26 19:30:06 +0200

    fix(nvim): update treesitter configuration (neovim 0.12+)

--- nvim/lua/crackcomm/treesitter/setup.lua
+++ nvim/lua/crackcomm/treesitter/setup.lua
@@ -1,72 +1,21 @@
-require("nvim-treesitter.configs").setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "python", "javascript", "typescript", "rust" },
+local ts = require("nvim-treesitter")
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+ts.setup({
+  install_dir = vim.fn.stdpath("data") .. "/site",
+  highlight = { enable = true },
+})
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+local ensure_installed =
+  { "c", "cpp", "lua", "vim", "vimdoc", "query", "go", "python", "javascript", "typescript", "rust" }
+ts.install(ensure_installed)
 
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = {},
+vim.treesitter.language.register(
+  "python",
+  { "py", "pyi", "pyx", "pxd", "pxi", "bzl", "BUILD", "BUILD.bazel", "WORKSPACE" }
+)
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  indent = {
-    enable = true,
-    disable = { "rust", "ocaml" },
-  },
-
-  highlight = {
-    enable = true,
-    disable = function(lang, buf)
-      local max_filesize = 1024 * 1024 -- 1 MB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { "markdown" },
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "si",
-      node_incremental = "sn",
-      scope_incremental = "sc",
-      node_decremental = "sv",
-    },
-  },
-
-  -- move = {
-  --   enable = true,
-  --   set_jumps = true, -- whether to set jumps in the jumplist
-  --   goto_next_start = {
-  --     ["]m"] = "@function.outer",
-  --     ["]]"] = "@class.outer",
-  --   },
-  --   goto_next_end = {
-  --     ["]M"] = "@function.outer",
-  --     ["]["] = "@class.outer",
-  --   },
-  --   goto_previous_start = {
-  --     ["[m"] = "@function.outer",
-  --     ["[["] = "@class.outer",
-  --   },
-  --   goto_previous_end = {
-  --     ["[M"] = "@function.outer",
-  --     ["[]"] = "@class.outer",
-  --   },
-  -- },
-
-  modules = {},
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
 })
--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     config = function()
       require("crackcomm.treesitter.setup")
     end,
   },
   -- "nvim-treesitter/nvim-treesitter-context",
-  "JoosepAlviste/nvim-ts-context-commentstring",
+  -- "JoosepAlviste/nvim-ts-context-commentstring", -- broken, unused
 }
