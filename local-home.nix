1438a3c572746f728a50ae8de94132004708f799
Łukasz Kurowski <crackcomm@gmail.com>
2026-05-16 18:06:38 +0200

    config(nix): enable bazel, common-tools, and lua

--- local-home.nix
+++ local-home.nix
@@ -1,10 +1,10 @@
 { ... }: {
   # Programming
-  local.bazel.enable = false; # true;
+  local.bazel.enable = true;
   local.common-libs.enable = false;
-  local.common-tools.enable = false; # true;
+  local.common-tools.enable = true;
   local.cpp.enable = false;
-  local.lua.enable = false; # true;
+  local.lua.enable = true;
   local.nodejs.enable = false;
   local.python.enable = false; # true;
   local.rust.enable = false;
