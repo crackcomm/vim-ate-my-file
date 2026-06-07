3788fd45658b678b2d7291b7abf1e52e9ccee971
Łukasz Kurowski <crackcomm@gmail.com>
2026-06-07 06:01:09 +0200

    chore(devenv): remove unused `committer.py` script

--- nix/features/devenv.nix
+++ nix/features/devenv.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   environment.systemPackages = [
-    (pkgs.python3.withPackages (ps:
-      [
-        ps.unidiff # dependency of our commit message generator
-      ]))
+    (pkgs.python3.withPackages (ps: [ ]))
     pkgs.tokn
   ];
 }
