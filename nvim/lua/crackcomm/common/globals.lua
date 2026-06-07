c8abcbdf9ec9a4dc7900dbc2b9b960c92329aeb3
Łukasz Kurowski <crackcomm@gmail.com>
2026-06-07 05:54:16 +0200

    feat(nvim): use `RX` require-reload meta-table

--- nvim/lua/crackcomm/common/globals.lua
+++ nvim/lua/crackcomm/common/globals.lua
@@ -14,6 +14,25 @@ R = function(name)
   return _require(name)
 end
 
+RX = function(name)
+  return setmetatable({}, {
+    __index = function(_, key)
+      return function(...)
+        local args = { n = select("#", ...), ... }
+        return function()
+          return R(name)[key](unpack(args, 1, args.n))
+        end
+      end
+    end,
+    __call = function(_, ...)
+      local args = { n = select("#", ...), ... }
+      return function()
+        return R(name)(unpack(args, 1, args.n))
+      end
+    end,
+  })
+end
+
 --- @diagnostic disable-next-line: lowercase-global
 errorf = function(msg, ...)
   error(string.format(msg, ...))
--- nvim/lua/crackcomm/jj/init.lua
+++ nvim/lua/crackcomm/jj/init.lua
@@ -6,51 +6,37 @@ local M = {}
 function M.setup()
   keymap.nmap({
     "<leader>sp",
-    function()
-      R("crackcomm.jj.squash").squash_to_parent()
-    end,
+    RX("crackcomm.jj.squash").squash_to_parent(),
     desc = "[jj] Squash to parent",
   })
   keymap.nmap({
     "<leader>snp",
-    function()
-      R("crackcomm.jj.squash").squash_to_new_parent()
-    end,
+    RX("crackcomm.jj.squash").squash_to_new_parent(),
     desc = "[jj] Squash to new parent",
   })
   keymap.nmap({
     "<leader>ht",
-    function()
-      R("crackcomm.jj.hunks").toggle_hunk()
-    end,
+    RX("crackcomm.jj.hunks").toggle_hunk(),
     desc = "[jj] Toggle hunk",
   })
   keymap.vmap({
     "<leader>ht",
-    function()
-      R("crackcomm.jj.hunks").add_selected_hunks()
-    end,
+    RX("crackcomm.jj.hunks").add_selected_hunks(),
     desc = "[jj] Toggle hunk",
   })
   keymap.nmap({
     "<leader>hc",
-    function()
-      R("crackcomm.jj.hunks").clear_marks()
-    end,
+    RX("crackcomm.jj.hunks").clear_marks(),
     desc = "[jj] Clear all hunks",
   })
   keymap.nmap({
     "<leader>dj",
-    function()
-      R("crackcomm.jj.describe").generate_commit_message()
-    end,
+    RX("crackcomm.jj.describe").generate_commit_message(),
     desc = "[jj] Describe commit with LLM",
   })
   keymap.nmap({
     "<leader>jl",
-    function()
-      a.void(R("crackcomm.jj.log"))()
-    end,
+    a.void(RX("crackcomm.jj.log")()),
     desc = "[jj] Short log",
   })
 end
