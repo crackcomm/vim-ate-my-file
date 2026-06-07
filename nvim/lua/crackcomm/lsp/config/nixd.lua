b1b664775528e91346afcf123626381e16c146f5
Łukasz Kurowski <crackcomm@gmail.com>
2026-06-07 05:59:27 +0200

    config(nvim/lsp): improve `nixd` configuration

--- nvim/lua/crackcomm/lsp/config/nixd.lua
+++ nvim/lua/crackcomm/lsp/config/nixd.lua
@@ -1,11 +1,23 @@
 return {
+  cmd = { "nixd", "--inlay-hints" },
   settings = {
     nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
       formatting = {
         command = { "nixfmt" },
       },
       options = {
-        enableExprDiagnostics = true,
+        nixos = {
+          expr = [[(let flake = builtins.getFlake (toString ./.); configs = flake.nixosConfigurations or { }; names = builtins.attrNames configs; in if names != [ ] then configs.${builtins.head names}.options else { })]],
+        },
+      },
+      diagnostic = {
+        suppress = { "sema-extra-with" },
+      },
+      inlayHints = {
+        enable = true,
       },
     },
   },
