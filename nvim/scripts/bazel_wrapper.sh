8355a8c92c63cf058ba6cacf4406ec004b8e2b72
Łukasz Kurowski <crackcomm@gmail.com>
2026-06-26 23:30:25 +0200

    refactor(nvim/gopackagesdriver): rely on the `BAZEL_WRAPPER` from environment

--- nvim/scripts/bazel_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Some `bazel run` actions like `:refresh_compile_commands` need to run
-# bazel itself.
-BAZEL_ACTION_PATH="/usr/bin:/bin:/run/current-system/sw/bin"
-
-env -i TERM=xterm-256color PATH="$BAZEL_ACTION_PATH" $BAZEL_REAL "$@"
--- nvim/scripts/gopackagesdriver.sh
+++ nvim/scripts/gopackagesdriver.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-this_script_dir="$(dirname $(realpath "$0"))"
-export BAZEL_WRAPPER="$this_script_dir/bazel_wrapper.sh"
+# If BAZEL_WRAPPER is not defined - exit with an error message
+if [ -z "$BAZEL_WRAPPER" ]; then
+  echo "Error: BAZEL_WRAPPER is not defined. Please run this script from within a Bazel build environment."
+  exit 1
+fi
 
 GOPACKAGESDRIVER_BIN=bazel-bin/external/rules_go+/go/tools/gopackagesdriver/gopackagesdriver_/gopackagesdriver
 
 if [ ! -x $GOPACKAGESDRIVER_BIN ]; then
-	exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}" || true
+  exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}" || true
 else
-	export BUILD_WORKSPACE_DIRECTORY=$(pwd)
-	export BUILD_WORKING_DIRECTORY=$(pwd)
-	exec $GOPACKAGESDRIVER_BIN "${@}" || true
+  export BUILD_WORKSPACE_DIRECTORY=$(pwd)
+  export BUILD_WORKING_DIRECTORY=$(pwd)
+  exec $GOPACKAGESDRIVER_BIN "${@}" || true
 fi
