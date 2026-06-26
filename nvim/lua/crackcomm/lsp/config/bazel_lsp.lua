90848c399b6f6ab116bea4b1bd1c620d81da3f8d
Łukasz Kurowski <crackcomm@gmail.com>
2026-06-26 23:32:42 +0200

    config(nvim/bazel_lsp): fix root directory detection for bazel-lsp

--- nvim/lua/crackcomm/lsp/config/bazel_lsp.lua
+++ nvim/lua/crackcomm/lsp/config/bazel_lsp.lua
@@ -1,5 +1,9 @@
 return {
   cmd = { "bazel-lsp" },
   filetypes = { "bzl" },
-  root_dir = vim.fn.getcwd(),
+  root_dir = function(bufnr, on_dir)
+    local filepath = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.dirname(vim.fs.find("WORKSPACE", { path = filepath, upward = true })[1])
+    on_dir(root or vim.fn.getcwd())
+  end,
 }
