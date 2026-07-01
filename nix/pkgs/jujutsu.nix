83dc170cffc5b53dd274265df7076abee9f31cc6
Łukasz Kurowski <crackcomm@gmail.com>
2026-07-01 11:04:27 +0200

    deps(nix/jj): update to `v0.42.0`

--- nix/pkgs/jujutsu.nix
+++ nix/pkgs/jujutsu.nix
@@ -5,22 +5,23 @@
 }:
 
 let
+  version = "0.42.0";
   system = stdenv.hostPlatform.system;
   systemToBinary = {
     "x86_64-linux" = {
-      name = "jj-v0.35.0-x86_64-unknown-linux-musl.tar.gz";
-      sha256 = "9967a240e3294a0bce4444c55d40a35b70af44c69b558689aced95e4e497cef2";
+      name = "jj-v${version}-x86_64-unknown-linux-musl.tar.gz";
+      sha256 = "2d91e81d649e617a81608e7401ad1106029c15ece01ac928c4a351abef42be6a";
     };
     "aarch64-linux" = {
-      name = "jj-v0.35.0-aarch64-unknown-linux-musl.tar.gz";
-      sha256 = "b42a8a102f60bf5e39d827a9a9faa92c763ca7c1a9cd7fd8782395bdedbecb76";
+      name = "jj-v${version}-aarch64-unknown-linux-musl.tar.gz";
+      sha256 = "bc962ac57ec264541a62ed8492f080898380a277222b115e1ed96163196e6fc8";
     };
   };
 
 in
 stdenv.mkDerivation rec {
   pname = "jj";
-  version = "0.35.0";
+  inherit version;
 
   src = fetchurl {
     url = "https://github.com/jj-vcs/jj/releases/download/v${version}/${systemToBinary.${system}.name}";
